e0e50aae07a1a1adefff9eb85201d0835e1f62c2
git-test <>
2026-05-10 17:34:11 +0900

    2026.5.10 17:34

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -3,6 +3,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,6 +15,16 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-
+        DB::table('users')->insert([
+        'over_name' => '内田',
+        'under_name' => '理沙',
+        'over_name_kana' => 'ウチダ',
+        'under_name_kana' => 'リサ',
+        'mail_address' => 'risauchida@example.com',
+        'sex' => '2',
+        'birth_day' => '1996-01-01',
+        'role' => '4',
+        'password' => Hash::make('012345678'),
+    ]);
     }
 }
